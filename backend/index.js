3e77db8387317ac3210db60a1278b5843d9430f5
Marcella LASA <marcella.amorimsa@gmail.com>
2020-01-18 22:22:22 -0300

    front web done

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv/config')
+const router = require('./src/router')
+const app = express()
+const cors = require('cors')
+
+mongoose.connect(
+	process.env.DB_CONNECTION,{ useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true,useFindAndModify: false, },
+	()=>{console.log('Connected to DB!')}
+)
+
+// app.use(cors({origin:'http://localhost:3000'}))  libera acesso so p/ app com esse endereco
+app.use(cors({}))//libera acesso p/ todas aplicacoes
+app.use(express.json())
+app.use(router)
+app.listen(8080,()=>{console.log('running at http://localhost:8080')})
---
+++ web/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+import api from './services/api';
+
+import DevForm from './components/DevForm/index'
+import DevItem from './components/DevItem/index'
+
+function App() {
+
+  const [devs,setDevs] = useState([]) //starts state with same type of data will be stored
+
+  useEffect(()=>{
+      async function loadDevs(){
+        const response = await api.get('/devs')
+        setDevs(response.data)
+        console.log(response.data)
+      }
+      loadDevs()
+    },[])
+
+  async function handleAddDev(data){
+    const response = await api.post('/devs', data)  
+    setDevs([...devs,response.data])
+  }
+  
+
+   return (
+    <div id="app">
+      <h1>heyDev</h1>
+      <div className="main-content">
+        <aside>
+          <strong>Cadastrar</strong>
+          <DevForm onSubmit={handleAddDev}/>
+        </aside>
+
+        <main>
+          <ul>
+            {devs.map(dev=>(<DevItem key={dev._id} dev={dev}/>  ))}
+          </ul>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
